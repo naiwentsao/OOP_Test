193ed22b9dbcaa05a89e90bddd9598b35fe9307b
naiwentsao <naiwentsao.mg11@nycu.edu.tw>
2025-04-21 20:14:54 +0800

    version1

--- src/Cylinder.cpp
+++ src/Cylinder.cpp
@@ -2,32 +2,41 @@
 # define CYLINDER_CPP
 
 # include "Cylinder.h"
+# include <cmath>
+# include <iomanip>
+using namespace std;
 
 double Cylinder :: SurfaceArea(){
     
-    return ;
+    double pi = M_PI;
+    
+    return 2.0*(double)this->radius*(double)this->radius*pi + 2.0*pi*(double)this->radius*(double)this->height;
 }
 
 double Cylinder :: Volume(){
-
-    return ;
+    double pi = M_PI;
+    
+    return (double)this->radius*(double)this->radius*pi*(double)this->height;
 }
 
 double Cylinder :: Circumference(){
-
-    return ;
+    double pi = M_PI;
+    
+    return (double)this->radius*pi*2.0;
 }
 
 istream & operator>>(istream & in, Cylinder & cldr)
 {
-
-    return ;
+    in >> cldr.radius >> cldr.height;
+    return in;
 }
 
 ostream & operator<<(ostream & out, Cylinder & cldr)
 {
-    
-    return ;
+    out << "Circumference: "<<fixed<<std::setprecision(3)<< cldr.Circumference() <<endl;
+    out << "SurfaceArea: "<<fixed<<std::setprecision(3)<< cldr.SurfaceArea()<<endl;
+    out << "Volume: "<<fixed<<std::setprecision(3)<< cldr.Volume();
+    return out;
 }
 
 # endif
